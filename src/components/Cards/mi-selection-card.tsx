320729fc6e9508c958ac69d237eab04be08568e5
Simon Davis <simonfreelance1@gmail.com>
2019-05-09 14:24:28 -0400

    comma added

--- src/components/Cards/mi-selection-card.tsx
+++ src/components/Cards/mi-selection-card.tsx
@@ -3,7 +3,7 @@ import { Component, Prop } from "@stencil/core";
 @Component({
   tag: "mi-selection-card",
   styleUrl: "./mi-selection-card.scss",
-  shadow: true
+  shadow: true,
 })
 export class SelectionCard {
   @Prop() description: string;
